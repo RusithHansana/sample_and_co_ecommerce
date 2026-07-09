74c59ead7c21af23936b012719fab7a57df6af10
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2026-07-09 19:10:18 +0530

    feat(server): initialize server with Express and TypeScript setup
    - Added package.json for server dependencies and scripts
    - Created index.ts for server entry point with health check endpoint
    - Configured TypeScript with tsconfig.json for strict type checking and module resolution

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express, { type Express, type Request, type Response, type NextFunction } from "express"
+
+const app: Express = express();
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+app.get('/api/health', (req: Request, res: Response) => {
+    res.send({data: {status: "ok"}});
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
+    res.status(500).send({ error: { message: err.message}});
+});
+
+app.listen(PORT, () => {
+    console.log(`server is running on port: ${PORT}`);
+});
