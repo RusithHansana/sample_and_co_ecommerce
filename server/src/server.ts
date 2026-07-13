571e6ba933697e46814f392bf5be807847f0a0b8
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2026-07-13 12:16:56 +0530

    feat(server): add logging for server startup with port information

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,7 @@
 import app from "./app.js";
 import { config } from "./config/index.js";
+import logger from "./lib/logger.js";
 
-app.listen(config.PORT);
+app.listen(config.PORT,() => {
+    logger.info(`Server is running on port:${config.PORT} `)
+});
