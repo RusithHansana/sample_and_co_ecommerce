2e1c8220ae1dc27366f92a9dacfb31f5bb47b3c1
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2026-07-13 12:03:04 +0530

    refactor(app): reorganize middleware setup and improve proxy trust configuration

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,15 +10,13 @@ import { notFoundHandler } from "./middleware/not-found-handler.js";
 import { limiter } from "./middleware/rate-limiter.js";
 
 const app: Express = express();
-app.use("/api/auth", express.Router());
-app.use("/api/products", express.Router());
-app.use("/api/cart", express.Router());
-app.use("/api/checkout", express.Router());
-app.use("/api/orders", express.Router());
-app.use("/api/reviews", express.Router());
-app.use("/api/admin", express.Router())
+
+// for getting the actual ip coming from "X-Forwarded-For" when using a reverse proxy.
+app.set("trust proxy", 1);
+
 app.use(requestLogger);
 app.use(helmet());
+
 app.use(cors({ 
     origin: (origin, callback) => {
         if(!origin || config.ALLOWED_ORIGIN.includes(origin)){
@@ -29,6 +27,7 @@ app.use(cors({
     }, 
     credentials: true
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
  
