46e51ed63a49e694d6735b4be2348c47e059dc05
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2026-07-13 19:32:53 +0530

    feat(prisma): implement Prisma client setup with PostgreSQL adapter and connection pooling

---
+++ server/src/lib/prisma.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+import { config } from "../config/index.js";
+
+export const pool = new Pool({
+    connectionString: config.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
