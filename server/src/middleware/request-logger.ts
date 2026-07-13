741ef99c7c43195f4bb02003683a5b902a4ca7b7
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2026-07-13 12:07:50 +0530

    fix(request-logger): normalize header casing for X-Request-Id

--- server/src/middleware/request-logger.ts
+++ server/src/middleware/request-logger.ts
@@ -6,13 +6,13 @@ import logger from "../lib/logger.js";
 export const requestLogger = pinoHttp({
     logger,
     genReqId: (req, res) => {
-        const existingId = req.headers["x-request-id"];
+        const existingId = req.headers["X-Request-Id"];
 
         if (existingId) return existingId as string;
 
         const id = randomUUID();
 
-        res.setHeader("x-request-id", id);
+        res.setHeader("X-Request-Id", id);
 
         return id;
     }
