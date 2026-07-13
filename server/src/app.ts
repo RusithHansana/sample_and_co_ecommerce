5e878f12d02da08573fcf5a400c0cb9b705914a2
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2026-07-13 11:16:45 +0530

    feat(auth): add rate limiting middleware to /api/auth route

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,9 +7,16 @@ import { config } from "./config/index.ts";
 import { requestLogger } from "./middleware/request-logger.ts";
 import { errorHandler } from "./middleware/error-handler.ts";
 import { notFoundHandler } from "./middleware/not-found-handler.ts";
+import { limiter } from "./middleware/rate-limiter.ts";
 
 const app: Express = express();
-
+app.use("/api/auth", express.Router());
+app.use("/api/products", express.Router());
+app.use("/api/cart", express.Router());
+app.use("/api/checkout", express.Router());
+app.use("/api/orders", express.Router());
+app.use("/api/reviews", express.Router());
+app.use("/api/admin", express.Router())
 app.use(requestLogger);
 app.use(helmet());
 app.use(cors({ origin: config.ALLOWED_ORIGIN, credentials: true}));
@@ -20,7 +27,7 @@ app.get('/api/health', (req: Request, res: Response) => {
     res.send({data: {status: "ok"}});
 });
 
-app.use("/api/auth", express.Router());
+app.use("/api/auth",limiter, express.Router());
 app.use("/api/products", express.Router());
 app.use("/api/cart", express.Router());
 app.use("/api/checkout", express.Router());
