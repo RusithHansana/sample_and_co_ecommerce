b007d134979a92cd240666fc5fe154d5732fd253
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2026-07-14 12:40:44 +0530

    feat(prisma): update HealthCheck model to use UUID for id and create new migration

--- server/prisma/migrations/20260714070939_init/migration.sql
+++ server/prisma/migrations/20260714070939_init/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "HealthCheck" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "HealthCheck_pkey" PRIMARY KEY ("id")
