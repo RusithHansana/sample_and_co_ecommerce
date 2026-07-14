14eecaf9b0ea1ea7f79258d77471a89540717a46
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2026-07-14 15:07:24 +0530

    feat(api): set default baseURL for axios instance to localhost if not provided

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosInstance, type AxiosResponse, type InternalAxiosRequestConfig } from "axios";
 
 const api: AxiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api",
     headers: {
         'Content-Type': 'application/json'
     }
