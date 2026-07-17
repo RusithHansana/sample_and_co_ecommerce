50e980d9fde746f5365da555aa845f66ea28d888
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2026-07-17 12:05:37 +0530

    refactor(errors): improve formatting and consistency in error class constructors

--- server/src/types/app-error.ts
+++ server/src/types/app-error.ts
@@ -2,7 +2,7 @@ export class AppError extends Error {
     public readonly code: string;
     public readonly statusCode: number;
 
-    constructor(message: string, code: string, statusCode: number){
+    constructor(message: string, code: string, statusCode: number) {
         super(message);
         this.name = this.constructor.name; // To differentiate (eg: when NotFoundError subclass throws an error the name will be "NotFoundError")
         this.code = code;
@@ -14,27 +14,33 @@ export class AppError extends Error {
 }
 
 export class NotFoundError extends AppError {
-    constructor(message: string = "Resource not found"){
+    constructor(message: string = "Resource not found") {
         super(message, "NOT_FOUND", 404);
     }
 }
 
 export class UnauthorizedError extends AppError {
-    constructor(message: string = "Unauthorized"){
+    constructor(message: string = "Unauthorized") {
         super(message, "UNAUTHORIZED", 401);
     }
 }
 
 export class ForbiddenError extends AppError {
-    constructor(message: string = "Forbidden"){
+    constructor(message: string = "Forbidden") {
         super(message, "FORBIDDEN", 403);
     }
 }
 
-export class ValidationError extends AppError {
-    public readonly details: {field: string, message: string} [];
+export class ConflictError extends AppError {
+    constructor(message: string = "Conflict Error") {
+        super(message, "CONFLICT", 409);
+    }
+}
 
-    constructor(message: string = "Validation Failed", details: {field:string, message: string}[] = []){
+export class ValidationError extends AppError {
+    public readonly details: { field: string, message: string }[];
+
+    constructor(message: string = "Validation Failed", details: { field: string, message: string }[] = []) {
         super(message, "VALIDATION_ERROR", 400);
         this.details = details
     }
