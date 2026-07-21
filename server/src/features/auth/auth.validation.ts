1b87ca4b7229ebde340714779e26d3ac2328156b
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2026-07-21 12:52:31 +0530

    fix(auth): update password validation message to specify character limits

--- server/src/features/auth/auth.validation.ts
+++ server/src/features/auth/auth.validation.ts
@@ -4,7 +4,7 @@ import { ValidationError } from "../../types/app-error.js";
 
 export const registerValidation = [
     body("email").isEmail().withMessage("Valid email is required").normalizeEmail(),
-    body("password").isLength({ min: 8 }).withMessage("Password must be atleast 8 characters"),
+    body("password").isLength({ min: 8, max: 72 }).withMessage("Password must be between 8 - 72 characters"),
     body("name").trim().notEmpty().withMessage("Name is required"),
 ];
 
