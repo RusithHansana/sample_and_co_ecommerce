8698aff45fdcf990e0028621345e0a44e53823b5
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2026-08-29 13:46:07 +0530

    feat: add ProductVariantDetailDTO and update ProductDetailDTO interface

--- server/src/types/product.ts
+++ server/src/types/product.ts
@@ -29,3 +29,21 @@ export interface ProductListItemDTO {
     averageRating: number;
     reviewCount: number;
 }
+
+export interface ProductVariantDetailDTO {
+    id: string;
+    attributes: Record<string, string>;
+    price: number;
+    stockStatus: "In Stock" | "Out of Stock";
+}
+
+export interface ProductDetailDTO {
+    id: string;
+    name: string;
+    descritpion: string;
+    images: string[];
+    category: string,
+    variants: ProductVariantDetailDTO[];
+    averageRating: number | null;
+    reviewCount: number;
+}
